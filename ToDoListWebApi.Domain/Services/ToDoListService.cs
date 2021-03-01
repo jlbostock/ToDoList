054e772e1cf6111dd7be2116baff2e548c26a757
Jean-Louis Bostock <jlouis.bostock@gmail.com>
2021-03-01 18:38:58 -0500

    added comments to ToDoListService

--- ToDoListWebApi.Domain/Services/ToDoListService.cs
+++ ToDoListWebApi.Domain/Services/ToDoListService.cs
@@ -21,21 +21,32 @@ namespace ToDoListWebApi.Domain.Services
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Add a new item to the to-do list repository
+        /// </summary>
+        /// <param name="newToDoItemVm"></param>
+        /// <exception cref="ArgumentNullException">If newToDoItemVm param is null</exception>
+        /// <returns>The new To-do item with the populated key</returns>
         public async Task<ToDoItemVm> AddToDoItem(ToDoItemVm newToDoItemVm)
         {
             if (newToDoItemVm is null) throw new ArgumentNullException(nameof(newToDoItemVm));
 
             if (!(newToDoItemVm.Id == default)) return null;
 
-            var newToDoItem = _mapper.Map<ToDoItem>(((ToDoItemVm)newToDoItemVm));
+            var newToDoItem = _mapper.Map<ToDoItem>(newToDoItemVm);
 
-            if (String.IsNullOrWhiteSpace(newToDoItem.TaskName)) return null;
+            if (string.IsNullOrWhiteSpace(newToDoItem.TaskName)) return null;
 
             var result = await _todoRepo.Add(newToDoItem);
 
             return _mapper.Map<ToDoItemVm>(result);
         }
 
+        /// <summary>
+        /// Find and remove a to-do item, from the repository, using the ID.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>The To-do item that was removed. Returns null if the to-do ID could not be found</returns>
         public async Task<ToDoItemVm> RemoveToDoItem(int id)
         {
             var result = await _todoRepo.Delete(id);
@@ -43,12 +54,20 @@ namespace ToDoListWebApi.Domain.Services
             return _mapper.Map<ToDoItemVm>(result);
         }
 
+        /// <summary>
+        /// Retrieve all To-do items from the repository.
+        /// </summary>
+        /// <returns>Enumerable of all To-do items</returns>
         public async Task<IEnumerable<ToDoItemVm>> GetAllToDoItems()
         {
             var result = await _todoRepo.GetAll();
             return _mapper.Map<IEnumerable<ToDoItemVm>>(result);
         }
 
+        /// <summary>
+        /// Removes all To-do items from the repository.
+        /// </summary>
+        /// <returns></returns>
         public async Task ClearList()
         {
             await _todoRepo.Clear();
